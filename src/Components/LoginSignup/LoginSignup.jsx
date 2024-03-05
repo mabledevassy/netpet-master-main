cd5161a4a373050d04fcb9411e23fa340b89c5ee
mabledevassy <mabledevassyt@gmail.com>
2024-03-05 21:26:27 +0530

    login

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -8,11 +8,11 @@ const LoginSignup = () => {
   var [inputs, setInputs] = useState({
     "Name": '',
     "Email": '',
-    "Password": '',
-    "Confirmpassword": ''
+    "Password": ''
+   
   })
 
-  const [passwordsMatch, setPasswordsMatch] = useState(true);
+
 
 
   const inputHandler = (event) => {
@@ -22,21 +22,17 @@ const LoginSignup = () => {
   }
 
   const addHandler = (event) => {
-    // console.log("Clicked")
-    event.preventDefault();
-    if (inputs.Password ===inputs.Confirmpassword) {
 
-      console.log(inputs)
+   console.log("Clicked")
+   console.log(inputs)
+   
       axios.post("http://localhost:3005/lnew", inputs)
         .then((response) => {
           alert("Signup Successfully")
         })
         .catch(err => console.log(err))
-    }
-    else {
-      setPasswordsMatch(false);
-    }
-
+  
+    
 
 
   }
@@ -48,15 +44,13 @@ const LoginSignup = () => {
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' id='Name' name='Name' value={inputs.Name} onChange={inputHandler} />
-          <input type="email" placeholder='Email Address' id='Email' name='Email' value={inputs.Email} onChange={inputHandler} />
-          <input type="password" placeholder='Password' id='Password' name='Password' value={inputs.Password} onChange={inputHandler} />
-          <input type="password" placeholder='Re-Enter Password' id='Confirmpassword' name='Confirmpassword' value={inputs.Confirmpassword} onChange={inputHandler} />
+          <input type="text" placeholder='Your Name'  name='Name' value={inputs.Name} onChange={inputHandler} />
+          <input type="email" placeholder='Email Address' name='Email' value={inputs.Email} onChange={inputHandler} />
+          <input type="password" placeholder='Password'  name='Password' value={inputs.Password} onChange={inputHandler} />
+         
 
         </div>
-        {!passwordsMatch && (
-          <p style={{ color: 'red' }}>Passwords do not match</p>
-        )}
+       
         <button onClick={addHandler}><Link to='/s'>Continue</Link></button>
         <p className="loginsignup-login">Already  have an account? <span><Link to='/l'>Login Here</Link></span></p>
         <div className="loginsignup-agree">
